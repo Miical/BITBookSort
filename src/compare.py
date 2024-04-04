1e720379bd067990924fee3ef77a3be4a974d256
Miical <879754743@qq.com>
2024-04-04 15:21:06 +0800

    Change partB and partC numbers to natural order

--- src/compare.py
+++ src/compare.py
@@ -13,13 +13,16 @@ def gen_comp_vec(parser_result):
 
     return comp_vec
 
-def single_charnum_comp(idx1, idx2):
+def single_charnum_comp(idx1, idx2, narture_order=False):
     if idx1.isdigit() ^ idx2.isdigit():
         return -1 if idx1.isdigit() else 1
 
+    if idx1.isdigit() and narture_order:
+        return int(idx1) - int(idx2)
+
     return -1 if idx1 < idx2 else 1
 
-def charnumstr_comp(idx1, idx2):
+def charnumstr_comp(idx1, idx2, nature_order=False):
     items1 = parse_charnum(idx1)
     items2 = parse_charnum(idx2)
 
@@ -27,13 +30,14 @@ def charnumstr_comp(idx1, idx2):
     min_len = min(len1, len2)
     for i in range(min_len):
         if items1[i] != items2[i]:
-            return single_charnum_comp(items1[i], items2[i])
+            return single_charnum_comp(items1[i], items2[i], nature_order)
     return len1 - len2
 
 def compvec_cmp(vec1, vec2):
-    for val1, val2 in zip(vec1, vec2):
+    for i in range(18):
+        val1, val2 = vec1[i], vec2[i]
         if val1 != val2:
-            return charnumstr_comp(val1, val2)
+            return charnumstr_comp(val1, val2, nature_order=(i>=6))
     return 0
 
 def bookidx_cmp(bookidx1, bookidx2):
